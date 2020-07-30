b9d079bcc4a8ab17296d26add2b3b703f7c46ea5
Julian Maingot <julianmaingot@gmail.com>
2020-07-29 22:34:25 -0700

    Added some error handling to node link

--- data-structures/linked-list.zig
+++ data-structures/linked-list.zig
@@ -7,11 +7,13 @@ pub fn LinkedList(comptime T: type) type {
             next: ?*Node = null,
             value: T,
 
+            const LinkingError = error.LinkingError;
+
             // sets next and prev for a node
-            pub fn link(self: *LinkedList(T).Node, prev: ?*LinkedList(T).Node, next: ?*LinkedList(T).Node) void {
-                // TODO error handling
-                assert(self.prev == null);
-                assert(self.next == null);
+            pub fn link(self: *LinkedList(T).Node, prev: ?*LinkedList(T).Node, next: ?*LinkedList(T).Node) !void {
+                if (self.prev != null or self.prev != null) {
+                    return error.LinkingError;
+                }
 
                 self.prev = prev;
                 self.next = next;
@@ -44,13 +46,13 @@ pub fn LinkedList(comptime T: type) type {
         };
 
         // add node to end
-        pub fn append(self: *LinkedList(T), elem: *Node) void {
-            self.insertAt(elem, self.len);
+        pub fn append(self: *LinkedList(T), elem: *Node) !void {
+            try self.insertAt(elem, self.len);
         }
 
         // add node to front
-        pub fn prepend(self: *LinkedList(T), elem: *Node) void {
-            self.insertAt(elem, 0);
+        pub fn prepend(self: *LinkedList(T), elem: *Node) !void {
+            try self.insertAt(elem, 0);
         }
 
         // removes node from list
@@ -110,16 +112,16 @@ pub fn LinkedList(comptime T: type) type {
         }
 
         // insert a node at the given index
-        pub fn insertAt(self: *LinkedList(T), elem: *Node, index: usize) void {
+        pub fn insertAt(self: *LinkedList(T), elem: *Node, index: usize) !void {
             // TODO exception instead
             assert(self.len >= index);
 
             if (index == 0) {
-                elem.link(null, self.head);
+                try elem.link(null, self.head);
                 self.head = elem;
             } else {
                 var prev = self.nodeAt(index - 1).?;
-                elem.link(prev, prev.next);
+                try elem.link(prev, prev.next);
             }
 
             if (index == self.len) {
@@ -161,12 +163,12 @@ test "append to linked list" {
         .value = 2,
     };
 
-    int_list.append(&node1);
+    try int_list.append(&node1);
     assert(int_list.len == 1);
     assert(int_list.head == &node1);
     assert(int_list.tail == &node1);
 
-    int_list.append(&node2);
+    try int_list.append(&node2);
     assert(int_list.len == 2);
     assert(int_list.head == &node1);
     assert(int_list.tail == &node2);
@@ -182,12 +184,12 @@ test "prepend to linked list" {
         .value = 2,
     };
 
-    int_list.prepend(&node1);
+    try int_list.prepend(&node1);
     assert(int_list.len == 1);
     assert(int_list.head == &node1);
     assert(int_list.tail == &node1);
 
-    int_list.prepend(&node2);
+    try int_list.prepend(&node2);
     assert(int_list.len == 2);
     assert(int_list.head == &node2);
     assert(int_list.tail == &node1);
@@ -206,9 +208,9 @@ test "remove from linked list" {
         .value = 3,
     };
 
-    int_list.append(&node1);
-    int_list.append(&node2);
-    int_list.append(&node3);
+    try int_list.append(&node1);
+    try int_list.append(&node2);
+    try int_list.append(&node3);
 
     int_list.remove(&node2);
     assert(int_list.len == 2);
@@ -238,9 +240,9 @@ test "get node at index" {
         .value = 3,
     };
 
-    int_list.append(&node1);
-    int_list.append(&node2);
-    int_list.append(&node3);
+    try int_list.append(&node1);
+    try int_list.append(&node2);
+    try int_list.append(&node3);
 
     assert(int_list.nodeAt(0) == &node1);
     assert(int_list.nodeAt(1) == &node2);
@@ -260,9 +262,9 @@ test "insert node at index" {
         .value = 3,
     };
 
-    int_list.insertAt(&node1, 0);
-    int_list.insertAt(&node2, 1);
-    int_list.insertAt(&node3, 1);
+    try int_list.insertAt(&node1, 0);
+    try int_list.insertAt(&node2, 1);
+    try int_list.insertAt(&node3, 1);
 
     assert(int_list.nodeAt(0) == &node1);
     assert(int_list.nodeAt(1) == &node3);
@@ -281,9 +283,9 @@ test "remove from index in linked list" {
         .value = 3,
     };
 
-    int_list.append(&node1);
-    int_list.append(&node2);
-    int_list.append(&node3);
+    try int_list.append(&node1);
+    try int_list.append(&node2);
+    try int_list.append(&node3);
 
     const elem = int_list.removeAt(1);
     assert(int_list.len == 2);
